b89bfb7fc757c4711d57de606f562edfa76427ed
mystique09 <knightkrypto7@gmail.com>
2023-04-08 00:23:34 +0800

    add(wip): app starter

--- src/bootstrap/application.v
+++ src/bootstrap/application.v
@@ -1,3 +1,26 @@
 module bootstrap
 
-pub struct Application {}
+struct Application {
+pub:
+	env Env
+	db  Database
+}
+
+pub fn new_app(env &Env) &Application {
+	db := new_db()
+	app := Application{
+		env: env
+		db: db
+	}
+	return &app
+}
+
+pub fn (app &Application) start_app() {
+	app.db.connect()
+	println('Starting application...')
+}
+
+pub fn (app &Application) stop_app() {
+	app.db.disconnect()
+	println('Gracefully shutting down...')
+}
