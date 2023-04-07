3725d11102409d7f5adf8e472fe2ee2475cd463a
mystique09 <knightkrypto7@gmail.com>
2023-04-08 00:23:21 +0800

    add(wip): db connector

--- src/bootstrap/database.v
+++ src/bootstrap/database.v
@@ -1,3 +1,17 @@
 module bootstrap
 
+// TODO!: connect to postgresql
 pub struct Database {}
+
+pub fn new_db() &Database {
+	db := Database{}
+	return &db
+}
+
+pub fn (db &Database) connect() {
+	println('Connecting database...')
+}
+
+pub fn (db &Database) disconnect() {
+	println('Gracefully shuttingdown database connection...')
+}
