cba96407d71e349eeb63e80cdc03d5a1e3bc360b
mystique09 <knightkrypto7@gmail.com>
2023-04-08 13:38:40 +0800

    fix: db connection

--- src/bootstrap/database.v
+++ src/bootstrap/database.v
@@ -8,20 +8,22 @@ pub struct Database {
 }
 
 pub fn new_db(env &Env) &Database {
-	config := db.Config{
-		host: env.db_host
-		port: env.db_port
-		user: env.db_username
-		password: env.db_pass
-		dbname: env.db_name
+	config := pg.Config
+	{
+		host:
+		env.db_host
+		port:
+		env.db_port.int()
+		user:
+		env.db_username
+		password:
+		env.db_pass
+		dbname:
+		env.db_name
 	}
-	conn := pg.connect(config)
+	conn := pg.connect(config) or { panic(err) }
 	db := Database{conn}
 	return &db
-}
-
-pub fn (db &Database) connect() {
-	println('Connecting database...')
 }
 
 pub fn (db &Database) disconnect() {
