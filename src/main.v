3f5e26d43db7a0ea875c61b5b98d46edf11348e9
mystique09 <knightkrypto7@gmail.com>
2023-04-08 15:19:36 +0800

    feat: router, ughhhhhh fckkkkkkkk

--- src/main.v
+++ src/main.v
@@ -1,14 +1,17 @@
 module main
 
 import bootstrap
+import api.router.v1
 
 fn main() {
-	env := bootstrap.new_env()
-	app := bootstrap.new_app(env)
+	app := bootstrap.new_app()
 	defer {
 		app.stop()
 	}
 
-	app.start()
+	mut route := v1.new_router()
+	route.setup_routes(app.db.conn)
+
+	app.start(route)
 	println('Hello, world!')
 }
