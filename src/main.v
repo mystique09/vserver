4115d1ccc0e3878f0eff76aa0ac41ed3add167ef
mystique09 <knightkrypto7@gmail.com>
2023-04-10 01:07:35 +0800

    chore: move route to app

--- src/main.v
+++ src/main.v
@@ -1,7 +1,6 @@
 module main
 
 import bootstrap
-import api.router.v1
 
 fn main() {
 	app := bootstrap.new_app()
@@ -9,9 +8,5 @@ fn main() {
 		app.stop()
 	}
 
-	mut route := v1.new_router()
-	route.setup_routes(app.db.conn)
-
-	app.start(route)
-	println('Hello, world!')
+	app.start()
 }
